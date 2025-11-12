04ce548ed3a21dfbb1ca8048fd1ed53bce459d40
Karolina Medovova <student.karolina@seznam.cz>
2025-11-12 22:37:11 +0100

    update

--- T2_playwright.dev.py
+++ T2_playwright.dev.py
@@ -19,16 +19,28 @@ assert page.url == "https://playwright.dev/", "❌ Nepodařilo se načíst strá
 print("---1. mezitest, kontrola načtení URL proběhla v pořádku---")
 
 print("6. Načteni Get started")
-page.click('text="Get started"')
+badge_get_started = page.locator('text="Get started"')
+badge_get_started.click()
 
 print("7. Načtení stránky Writing tests")
 page.click('text="Writing tests"')
+
+"""
+tady přichází zajímavost, že když jsem bod výše zkusila takto:
+badge2 = page.locator('text="Writing tests"')
+badge2.click()
+
+hodilo to dlouhou chybu, končící  :Call log:
+                                  - waiting for locator("text=\"Writing tests\"")
+a tomu bych ráda porozuměla. stejný lokátor, jen jinak napsaný, a objeví se problém.
+"""
 
 assert page.url == "https://playwright.dev/docs/writing-tests", "❌ Nepodařilo se načíst stránku"
 print("---2. mezitest, kontrola načtení URL proběhla v pořáku---")
 
 print("8. Načtení stránky How to write the first test")
-page.click('text="How to write the first test"')
+badge3 = page.locator('text="How to write the first test"')
+badge3.click()
 
 assert page.url == "https://playwright.dev/docs/writing-tests#first-test", "❌ Test neprošel!"
 print("--- ✅ CELÝ TEST PROBĚHL ÚSPĚŠNĚ!=== ")

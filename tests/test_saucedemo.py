7dfb81f045a49e929a2d0e436b850c61f6d15cac
Karolina Medovova <student.karolina@seznam.cz>
2026-01-22 23:26:22 +0100

    Update test_saucedemo.

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,13 +1,14 @@
 from playwright.sync_api import expect
 
+# test zobrazení titulku :
 def test_saucedemo_title(page):
     page.goto("https://www.saucedemo.com/")
     title = page.get_by_text("Swag Labs")
     expect(title).to_be_visible()
 
+# test přihlášení a načtení stránky s produkty, vložení do košíku, 
 def test_saucedemo(page):
     page.goto("https://www.saucedemo.com/")
-    # page.pause()
     username = page.get_by_role("textbox", name = "Username")
     username.fill("standard_user")
     password = page.get_by_role("textbox", name = "Password")
@@ -16,13 +17,15 @@ def test_saucedemo(page):
     expect(login).to_be_visible()
     login.click()
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
-    # page.pause()
     products = page.locator(".inventory_item_name")
-    expect(products).to_have_count(6)                       
-    # to have count ověřuje i to, že jsou prvky viditelné, tudíž není potřeba ověřovat pomocí .to_be_visible()
+    expect(products).to_have_count(6)                     # to have count ověřuje i to, že jsou prvky viditelné, tudíž není potřeba ověřovat pomocí .to_be_visible()
     price = page.locator(".inventory_item_price")
     expect(price).to_have_count(6)
     add_to_cart = page.get_by_role("button", name = "Add to cart")
     expect(add_to_cart).to_have_count(6)
-
-
+    sort_by_price = page.locator("[data-test=\"product-sort-container\"]").select_option("lohi")
+    backpack = page.locator("[data-test=\"item-4-title-link\"]").click()
+    expect(page).to_have_url("https://www.saucedemo.com/inventory-item.html?id=4")
+    add_backpack = page.locator("[data-test=\"add-to-cart\"]").click()
+    cart = page.locator("[data-test=\"shopping-cart-link\"]").click()
+    expect(page).to_have_url("https://www.saucedemo.com/cart.html")

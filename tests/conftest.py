4e71f24048e5f9bfa8cd7d1fada3216ecdf62479
Karolina Medovova <student.karolina@seznam.cz>
2026-01-30 23:39:14 +0100

    Vytvoření conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+#from dotenv import load_dotenv
+#load_dotenv()
+from test_saucedemo import login
+
+
+# fixtura pro login a zobrazení úvodní stránky:
+@pytest.fixture(scope="function")
+def logged_in_page(page):
+    login(page)
+    return page
